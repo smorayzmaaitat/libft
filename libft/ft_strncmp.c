b2a236c734b05f56775fb1bbb8001840a1689d59
ismailmaaitat <65847371+smorayzmaaitat@users.noreply.github.com>
2022-10-23 17:47:27 +0100

    Add files via upload

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imaaitat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/07 17:45:09 by imaaitat          #+#    #+#             */
+/*   Updated: 2022/10/07 17:52:45 by imaaitat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while ((unsigned char)s1[i] == (unsigned char)s2[i]
+		&& s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
+		i++;
+	if (n > 0)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	else
+		return (0);
+}
